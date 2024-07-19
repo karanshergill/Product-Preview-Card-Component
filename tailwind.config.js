c4e7891a2c5f287140fbdcbaef48f4f0f3e406d7
Karan <email@karanshergill.com>
2024-07-20 01:58:05 +0530

    desktop view code

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./index.html"
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'Primary-Dark-Cyan': 'hsl(158, 36%, 37%)',
+        'Primary-Cream': 'hsl(30, 38%, 92%)',
+        'Neutral-Very-Dark-Blue': 'hsl(212, 21%, 14%)',
+        'Neutral-Dark-Grayish-Blue': 'hsl(228, 12%, 48%)',
+        'Neutral-White': 'hsl(0, 0%, 100%)'
+      },
+      fontFamily: {
+        'Montserrat': ['Montserrat', 'sans-serif'],
+        'Fraunces': ['Fraunces','serif']
+      }
+    },
   },
   plugins: [],
 }
